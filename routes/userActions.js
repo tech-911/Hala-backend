9e660e114eec0dd219dfaf32e517129da04385a9
tech-911 <babatundeolatunji74@gmail.com>
2023-05-23 19:56:44 +0100

    ...updating

--- routes/userActions.js
+++ routes/userActions.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const verify = require("../verification/verifyToken");
 const { GetAllUsersByGender } = require("../controllers/userActionsController");
 
-router.post("/getallusersbygender", verify, GetAllUsersByGender);
+router.post("/getallusersbygender", verify, GetAllUsersByGender); //get users by gender
 
 module.exports = router;
