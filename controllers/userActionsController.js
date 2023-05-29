2fa4839f48a34b91c1404976ba639892b4c7a4da
tech-911 <babatundeolatunji74@gmail.com>
2023-05-29 11:39:42 +0100

    ...added user action for like and unlike profile

--- controllers/userActionsController.js
+++ controllers/userActionsController.js
@@ -1,9 +1,9 @@
 const { User } = require("../model/User");
-
+const { UserAction } = require("../model/UserActions");
 
 const GetAllUsersByGender = async (req, res) => {
   const { gender } = req.body;
-  User.find({ gender: gender}, (err, docs) => {
+  User.find({ gender: gender }, (err, docs) => {
     if (err) {
       res.status(400).send(err);
     } else {
@@ -11,5 +11,86 @@ const GetAllUsersByGender = async (req, res) => {
     }
   });
 };
+const Reacted = async (req, res) => {
+  const { email, reactEmail, action } = req.body;
 
-module.exports = { GetAllUsersByGender };
+  const actionResponse = await UserAction.findOne({ email: email });
+  console.log(actionResponse);
+  if (actionResponse) {
+    if (action === "like") {
+      if (actionResponse.liked.includes(reactEmail)) {
+        return res.status(400).send({ message: "user already liked" });
+      } else {
+        if (actionResponse.unlike.includes(reactEmail)) {
+          const index = actionResponse.unlike.indexOf(reactEmail);
+          if (index > -1) {
+            actionResponse.unlike.splice(index, 1);
+          }
+          actionResponse.liked.push(reactEmail);
+          actionResponse.save();
+          console.log(actionResponse);
+          res.send({ message: "reacted sucessfully" });
+        } else {
+          actionResponse.liked.push(reactEmail);
+          actionResponse.save();
+          console.log(actionResponse);
+          res.send({ message: "reacted sucessfully" });
+        }
+      }
+    } else if (action === "unlike") {
+      if (actionResponse.unlike.includes(reactEmail)) {
+        return res.status(400).send({ message: "user already unliked" });
+      } else {
+        if (actionResponse.liked.includes(reactEmail)) {
+          const index = actionResponse.liked.indexOf(reactEmail);
+          if (index > -1) {
+            actionResponse.liked.splice(index, 1);
+          }
+          actionResponse.unlike.push(reactEmail);
+          actionResponse.save();
+          console.log(actionResponse);
+          res.send({ message: "reacted sucessfully" });
+        } else {
+          actionResponse.unlike.push(reactEmail);
+          actionResponse.save();
+          console.log(actionResponse);
+          res.send({ message: "reacted sucessfully" });
+        }
+      }
+    } else {
+      return res.status(400).send({ message: "incorrect react action" });
+    }
+  } else {
+    if (action === "like") {
+      try {
+        const userActionRes = await new UserAction({
+          email: email,
+          liked: [reactEmail],
+          unlike: [],
+        });
+        const saveValue = await userActionRes.save();
+        console.log(saveValue);
+        res.send(saveValue);
+      } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+      }
+    } else if (action === "unlike") {
+      try {
+        const userActionRes = await new UserAction({
+          email: email,
+          liked: [],
+          unlike: [reactEmail],
+        });
+        const saveValue = await userActionRes.save();
+        console.log(saveValue);
+        res.send(saveValue);
+      } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+      }
+    }
+  }
+};
+
+module.exports = { GetAllUsersByGender, Reacted };
--- routes/userActions.js
+++ routes/userActions.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const verify = require("../verification/verifyToken");
-const { GetAllUsersByGender } = require("../controllers/userActionsController");
+const {
+  GetAllUsersByGender,
+  Reacted,
+} = require("../controllers/userActionsController");
 
 router.post("/getallusersbygender", verify, GetAllUsersByGender); //get users by gender
+router.post("/reacted", verify, Reacted); //get users by gender
 
 module.exports = router;
